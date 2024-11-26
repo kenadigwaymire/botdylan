cdde6a34eaddcf5e02d7007eea6a0d1289db0616
Camilo8274 <90429898+Camilo8274@users.noreply.github.com>
2024-11-26 12:06:30 -0800

    msg

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob('launch/*')),
         ('share/' + package_name + '/urdf', glob('urdf/*')),
-        ('share/' + package_name + '/meshes', glob('meshes/*')),  # Include meshes if applicable
+        ('share/' + package_name + '/meshes', glob('meshes/**/*.dae', recursive=True)),  # Include meshes if applicable
         ('share/' + package_name + '/code', glob('code/*'))
     ],
     install_requires=['setuptools'],
