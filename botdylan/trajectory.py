a5fcaeca0fbc7d063fec0bfa2fae07050e82e592
Kenadi Waymire <kwaymire@caltech.edu>
2024-11-25 23:43:29 -0800

    m

--- botdylan/trajectory.py
+++ botdylan/trajectory.py
@@ -112,20 +112,23 @@ class Trajectory():
         # (pd, vd) = goto(t, T, nextChord, prevChord)
         # xddot = vd
 
-        [ptips, Rtips, Jv, Jw] = np.vstack((
-            self.rh_pointer.fkin(self.qd[0:6]), 
-            self.rh_middle.fkin(np.concatenate((self.qd[0:2],self.qd[6:10]))), 
-            self.rh_ring.fkin(np.concatenate((self.qd[0:2],self.qd[10:14]))), 
-            self.rh_pinky.fkin(np.concatenate((self.qd[0:2],self.qd[14:19]))), 
-            self.rh_thumb.fkin(np.concatenate((self.qd[0:2],self.qd[19:24]))), 
+        print(f'\ntest self.qd[0:6]:\n {self.qd[0:6]}')
+        print(f'\ntest self.rh_pointer.fkin(self.qd[0:6]):\n {self.rh_pointer.fkin(self.qd[0:6])}')
 
-            self.lh_pointer.fkin(self.qd[24:30]), 
-            self.lh_middle.fkin(np.concatenate((self.qd[24:26],self.qd[30:34]))), 
-            self.lh_ring.fkin(np.concatenate((self.qd[24:26],self.qd[34:38]))), 
-            self.lh_pinky.fkin(np.concatenate((self.qd[24:26],self.qd[38:43]))), 
-            self.lh_thumb.fkin(np.concatenate((self.qd[24:26],self.qd[43:48]))), 
-            ))
-        J = np.vstack((Jv, Jw))
+        # [ptips, Rtips, Jv, Jw] = np.vstack((
+        #     self.rh_pointer.fkin(self.qd[0:6]), 
+        #     self.rh_middle.fkin(np.concatenate((self.qd[0:2],self.qd[6:10]))), 
+        #     self.rh_ring.fkin(np.concatenate((self.qd[0:2],self.qd[10:14]))), 
+        #     self.rh_pinky.fkin(np.concatenate((self.qd[0:2],self.qd[14:19]))), 
+        #     self.rh_thumb.fkin(np.concatenate((self.qd[0:2],self.qd[19:24]))), 
+
+        #     self.lh_pointer.fkin(self.qd[24:30]), 
+        #     self.lh_middle.fkin(np.concatenate((self.qd[24:26],self.qd[30:34]))), 
+        #     self.lh_ring.fkin(np.concatenate((self.qd[24:26],self.qd[34:38]))), 
+        #     self.lh_pinky.fkin(np.concatenate((self.qd[24:26],self.qd[38:43]))), 
+        #     self.lh_thumb.fkin(np.concatenate((self.qd[24:26],self.qd[43:48]))), 
+        #     ))
+        # J = np.vstack((Jv, Jw))
         
         # errp = ep(self.pdlast, ptips)
         # # errR = eR(self.Rdlast, Rtips)
