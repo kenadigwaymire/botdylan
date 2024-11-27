a7bb16a4cac40c1f59048ac59df7083f382fcd00
Camilo8274 <90429898+Camilo8274@users.noreply.github.com>
2024-11-26 19:27:23 -0800

    msg

--- botdylan/trajectory.py
+++ botdylan/trajectory.py
@@ -179,7 +179,7 @@ class Trajectory():
                                     lh_ff_Jw, lh_mf_Jw, lh_rf_Jw, lh_lf_Jw, 
                                     lh_th_Jw))]
         
-        J = np.vstack((Jv, Jw))
+        J = np.transpose(np.vstack((Jv, Jw)))
         Jpinv = np.linalg.pinv(J)
         print(f'\nJpinv:\n {Jpinv}\n')
         
