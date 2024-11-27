43d3f7672751dc0382a625d2e4327fb9778790bb
Kenadi Waymire <kwaymire@caltech.edu>
2024-11-26 17:47:51 -0800

    created new pkg and replaced files

--- setup.py
+++ setup.py
@@ -1,40 +1,25 @@
-import os
-from glob import glob
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 package_name = 'botdylan'
-
-# Helper function to recursively collect files in subdirectories
-def recursive_files(path):
-    files = []
-    for dirpath, _, filenames in os.walk(path):
-        for f in filenames:
-            files.append(os.path.join(dirpath, f))
-    return files
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(include=['botdylan', 'botdylan.*'], exclude=['test']),
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob('launch/*')),
-        ('share/' + package_name + '/urdf', glob('urdf/*')),
-        ('share/' + package_name + '/meshes', glob('meshes/*.dae', recursive=True)),  # Include meshes if applicable
-        ('share/' + package_name + '/code', glob('code/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='robot',
-    maintainer_email='robot@todo.todo',
-    description='Bot Dylan Robot',
+    maintainer='trey',
+    maintainer_email='firehawk4427@gmail.com',
+    description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'trajectory = botdylan.trajectory:main',
         ],
     },
 )
