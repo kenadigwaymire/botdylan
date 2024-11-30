453ccb877ae1798d3e9f11187f401a98ab142c72
Camilo8274 <90429898+Camilo8274@users.noreply.github.com>
2024-11-30 01:30:20 -0800

    msg

--- botdylan/trajectory.py
+++ botdylan/trajectory.py
@@ -81,18 +81,18 @@ class Trajectory():
                             self.jointnames()[24:26]+self.jointnames()[43:48])
         
         # Init joint values (doesnt work rn cause chain is <6 and len(jointnames) = 48)
-        self.qd = np.zeros(len(self.jointnames())) # which is declared first?
-        self.p0 = np.array([0.0, 0.0, 0.0,
-                            0.1, 0.0, 0.0,
-                            0.2, 0.0, 0.0,
-                            0.3, 0.0, 0.0,
-                            0.4, 0.0, 0.0,
+        self.q0 =  # write the initial joint positions here
+        # self.p0 = np.array([0.0, 0.0, 0.0,
+        #                     0.1, 0.0, 0.0,
+        #                     0.2, 0.0, 0.0,
+        #                     0.3, 0.0, 0.0,
+        #                     0.4, 0.0, 0.0,
 
-                            12.0, 0.0, 0.0,
-                            12.1, 0.0, 0.0,
-                            12.2, 0.0, 0.0,
-                            12.3, 0.0, 0.0,
-                            12.4, 0.0, 0.0])
+        #                     12.0, 0.0, 0.0,
+        #                     12.1, 0.0, 0.0,
+        #                     12.2, 0.0, 0.0,
+        #                     12.3, 0.0, 0.0,
+        #                     12.4, 0.0, 0.0])
         self.R0 = np.hstack((Reye(), Reye(), Reye(), Reye(), Reye(), 
                              Reye(), Reye(), Reye(), Reye(), Reye()))
 
@@ -105,27 +105,29 @@ class Trajectory():
     def jointnames(self):
         # Return a list of joint names FOR THE EXPECTED URDF!
         return [
-            # -------------------- Right Hand --------------------
-            # wrist
-            "rh_WRJ2", "rh_WRJ1",
-            "rh_FFJ4", "rh_FFJ3", "rh_FFJ2", "rh_FFJ1",
-            "rh_MFJ4", "rh_MFJ3", "rh_MFJ2", "rh_MFJ1",
-            "rh_RFJ4", "rh_RFJ3", "rh_RFJ2", "rh_RFJ1",
-            "rh_LFJ5", "rh_LFJ4", "rh_LFJ3", "rh_LFJ2", "rh_LFJ1",
-            "rh_THJ5", "rh_THJ4", "rh_THJ3", "rh_THJ2", "rh_THJ1",
-            # -------------------- Left Hand ---------------------
-            # wrist
-            "lh_WRJ2", "lh_WRJ1",
-            "lh_FFJ4", "lh_FFJ3", "lh_FFJ2", "lh_FFJ1",
-            "lh_MFJ4", "lh_MFJ3", "lh_MFJ2", "lh_MFJ1",
-            "lh_RFJ4", "lh_RFJ3", "lh_RFJ2", "lh_RFJ1",
-            "lh_LFJ5", "lh_LFJ4", "lh_LFJ3", "lh_LFJ2", "lh_LFJ1",
-            "lh_THJ5", "lh_THJ4", "lh_THJ3", "lh_THJ2", "lh_THJ1"
+            # -------------------- Right Hand (STRUMMING) --------------------
+            "rh_WRJ2", "rh_WRJ1", # wrist
+            "rh_FFJ4", "rh_FFJ3", "rh_FFJ2", "rh_FFJ1", # pointer
+            "rh_MFJ4", "rh_MFJ3", "rh_MFJ2", "rh_MFJ1", # middle
+            "rh_RFJ4", "rh_RFJ3", "rh_RFJ2", "rh_RFJ1", # ring
+            "rh_LFJ5", "rh_LFJ4", "rh_LFJ3", "rh_LFJ2", "rh_LFJ1", # pinky
+            # We won't be using the right hand thumb — we set these to fixed joints
+            # "rh_THJ5", "rh_THJ4", "rh_THJ3", "rh_THJ2", "rh_THJ1",
+            # -------------------- Left Hand (FRETTING) ----------------------
+            "right_hand_to_left_hand",  # prismatic joint for sliding along the neck
+            "lh_WRJ2", "lh_WRJ1", # wrist
+            "lh_FFJ4", "lh_FFJ3", "lh_FFJ2", "lh_FFJ1", # pointer
+            "lh_MFJ4", "lh_MFJ3", "lh_MFJ2", "lh_MFJ1", # middle
+            "lh_RFJ4", "lh_RFJ3", "lh_RFJ2", "lh_RFJ1", # ring
+            "lh_LFJ5", "lh_LFJ4", "lh_LFJ3", "lh_LFJ2", "lh_LFJ1", # pinky
+            "lh_THJ5", # Keeping one joint to pivot thumb up and down to stay below guitar neck when wrist moves
+            # Made these fixed:
+            # "lh_THJ4", "lh_THJ3", "lh_THJ2", "lh_THJ1"
             ]
     
     # Evaluate at the given time.  This was last called (dt) ago.
     def evaluate(self, t, dt):
-        # Initializae a guitar with: 21 frets, spaced 1 inch apart, and 
+        # Initialize a guitar with: 21 frets, spaced 1 inch apart, and 
         # 6 strings spaced 0.5 inches apart, at  a height of 0
         fretboard = Fretboard(21, 1, 0.5, 0)
 
@@ -144,38 +146,39 @@ class Trajectory():
         [rh_mf_ptip, rh_mf_Rtip, rh_mf_Jv, rh_mf_Jw] = self.rh_middle.fkin(np.concatenate((self.qd[0:2],self.qd[6:10])))
         [rh_rf_ptip, rh_rf_Rtip, rh_rf_Jv, rh_rf_Jw] = self.rh_ring.fkin(np.concatenate((self.qd[0:2],self.qd[10:14])))
         [rh_lf_ptip, rh_lf_Rtip, rh_lf_Jv, rh_lf_Jw] = self.rh_pinky.fkin(np.concatenate((self.qd[0:2],self.qd[14:19])))
-        [rh_th_ptip, rh_th_Rtip, rh_th_Jv, rh_th_Jw] = self.rh_thumb.fkin(np.concatenate((self.qd[0:2],self.qd[19:24])))
+        # We don't need the right hand thumb — we set these to fixed joints
+        # [rh_th_ptip, rh_th_Rtip, rh_th_Jv, rh_th_Jw] = self.rh_thumb.fkin(np.concatenate((self.qd[0:2],self.qd[19:24])))
 
-        [lh_ff_ptip, lh_ff_Rtip, lh_ff_Jv, lh_ff_Jw] = self.lh_pointer.fkin(self.qd[24:30])
-        [lh_mf_ptip, lh_mf_Rtip, lh_mf_Jv, lh_mf_Jw] = self.lh_middle.fkin(np.concatenate((self.qd[24:26],self.qd[30:34])))
-        [lh_rf_ptip, lh_rf_Rtip, lh_rf_Jv, lh_rf_Jw] = self.lh_ring.fkin(np.concatenate((self.qd[24:26],self.qd[34:38])))
-        [lh_lf_ptip, lh_lf_Rtip, lh_lf_Jv, lh_lf_Jw] = self.lh_pinky.fkin(np.concatenate((self.qd[24:26],self.qd[38:43])))
-        [lh_th_ptip, lh_th_Rtip, lh_th_Jv, lh_th_Jw] = self.lh_thumb.fkin(np.concatenate((self.qd[24:26],self.qd[43:48]))) 
+        [lh_ff_ptip, lh_ff_Rtip, lh_ff_Jv, lh_ff_Jw] = self.lh_pointer.fkin(self.qd[19:26])
+        [lh_mf_ptip, lh_mf_Rtip, lh_mf_Jv, lh_mf_Jw] = self.lh_middle.fkin(np.concatenate((self.qd[19:22],self.qd[26:30])))
+        [lh_rf_ptip, lh_rf_Rtip, lh_rf_Jv, lh_rf_Jw] = self.lh_ring.fkin(np.concatenate((self.qd[19:22],self.qd[30:34])))
+        [lh_lf_ptip, lh_lf_Rtip, lh_lf_Jv, lh_lf_Jw] = self.lh_pinky.fkin(np.concatenate((self.qd[19:22],self.qd[34:39])))
+        [lh_th_ptip, lh_th_Rtip, lh_th_Jv, lh_th_Jw] = self.lh_thumb.fkin(np.concatenate((self.qd[19:22],self.qd[39]))) 
 
         [ptips, Rtips, errR, Jv, Jw] = [np.hstack((rh_ff_ptip, rh_mf_ptip, 
-                                    rh_rf_ptip, rh_lf_ptip, rh_th_ptip, 
+                                    rh_rf_ptip, rh_lf_ptip, 
                                     lh_ff_ptip, lh_mf_ptip, lh_rf_ptip, 
                                     lh_lf_ptip, lh_th_ptip)),
                                  np.hstack((rh_ff_Rtip, rh_mf_Rtip, 
-                                    rh_rf_Rtip, rh_lf_Rtip, rh_th_Rtip, 
+                                    rh_rf_Rtip, rh_lf_Rtip, 
                                     lh_ff_Rtip, lh_mf_Rtip, lh_rf_Rtip, 
                                     lh_lf_Rtip, lh_th_Rtip)),
                                  np.hstack((eR(self.Rdlast, rh_ff_Rtip),
                                             eR(self.Rdlast, rh_mf_Rtip), 
                                             eR(self.Rdlast, rh_rf_Rtip), 
                                             eR(self.Rdlast, rh_lf_Rtip), 
-                                            eR(self.Rdlast, rh_th_Rtip), 
+                                            eR(self.Rdlast), 
                                             eR(self.Rdlast, lh_ff_Rtip), 
                                             eR(self.Rdlast, lh_mf_Rtip), 
                                             eR(self.Rdlast, lh_rf_Rtip), 
                                             eR(self.Rdlast, lh_lf_Rtip), 
                                             eR(self.Rdlast, lh_th_Rtip))),
                                  np.hstack((rh_ff_Jv, rh_mf_Jv, rh_rf_Jv, 
-                                    rh_lf_Jv, rh_th_Jv, 
+                                    rh_lf_Jv,
                                     lh_ff_Jv, lh_mf_Jv, lh_rf_Jv, lh_lf_Jv, 
                                     lh_th_Jv)),
                                  np.hstack((rh_ff_Jw, rh_mf_Jw, rh_rf_Jw, 
-                                    rh_lf_Jw, rh_th_Jw, 
+                                    rh_lf_Jw,
                                     lh_ff_Jw, lh_mf_Jw, lh_rf_Jw, lh_lf_Jw, 
                                     lh_th_Jw))]
         
