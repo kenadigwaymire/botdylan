666ea2d752aac76a54427e7a9eeb50ac3ca83101
Kenadi Waymire <kwaymire@caltech.edu>
2024-12-07 17:55:40 -0800

    Fixed

--- botdylan/trajectory.py
+++ botdylan/trajectory.py
@@ -285,7 +285,7 @@ class Trajectory():
         vd = np.concatenate((rh_vd, lh_vd))
 
         print(f'\nprevChord:\n {prevChord}\n')
-        if nextChord:
+        if nextChord != None:
             print(f'\nnextChord:\n {nextChord}\n')
         else:
             print('\nnextChord not defined\n')
